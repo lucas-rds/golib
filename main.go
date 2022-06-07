4de1c4ab7bd38982053943d3053f6373e1121693
Lucas Santos <lucas.rodrigues6673@gmail.com>
2022-06-07 19:51:11 -0300

    2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package golib
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -10,4 +11,8 @@ func TesteLib(echoInstance *echo.Echo) {
 	echoInstance.GET("/lib", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]string{"A": "B"})
 	})
+}
+
+func NovoMetodo() {
+	fmt.Println("NOVO METODO")
 }
