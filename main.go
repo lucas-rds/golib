1a8cdffec8fcfeced2b4bcfadd88d9b3c8c3a708
lucas-rds <lucas.rodrigues6673@gmail.com>
2022-06-07 19:20:08 -0300

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package golib
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func TesteLib(echoInstance echo.Echo) {
+	echoInstance.GET("/lib", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"A": "B"})
+	})
+}
